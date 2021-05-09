4ea922f183521030d2cc3865ea64064c52b58370
Santiago Feijoo <santiago.feijoo96@gmail.com>
2021-05-09 18:41:10 -0300

    Agregado el sv de escucha para sabotaje, y el puerto en los config de imongo y discorddiador

--- discordiador/include/discordiador.h
+++ discordiador/include/discordiador.h
@@ -3,25 +3,33 @@
 #ifndef DISCORDIADOR_H
 #define DISCORDIADOR_H
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<commons/log.h>
-#include<commons/string.h>
-#include<commons/config.h>
-#include<readline/readline.h>
+#define MAX_CLIENTS 128
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <commons/log.h>
+#include <commons/string.h>
+#include <commons/config.h>
+#include <readline/readline.h>
 
 #include "shared_utils.h"
 
 int conexion_mi_ram_hq;
+t_log *logger;
+t_config *config;
 
 //tarea tarea1;
 //tarea tarea2;
 
-t_log* iniciar_logger(void);
-t_config* leer_config(void);
-void leer_consola(t_log*);
-void leer_consola_prueba(t_log*);
+t_log *iniciar_logger(void);
+t_config *leer_config(void);
+void leer_consola(t_log *);
+void leer_consola_prueba(t_log *);
 void paquete(int);
-void terminar_programa(int,int, t_log*, t_config*);
+void terminar_programa(int, int, t_log *, t_config *);
+void *esperar_conexiones(int);
+int iniciar_servidor_discordiador(int);
+void crear_hilo_para_manejar_suscripciones(t_list *, int);
+void *manejar_suscripciones_discordiador(int *);
 
 #endif /* DISCORDIADOR_H */
