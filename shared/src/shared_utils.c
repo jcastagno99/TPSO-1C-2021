401c3bf80b831bca5a6f59a46537424a2c8a40c0
Damian Teplitz <dteplitz@frba.utn.edu.ar>
2021-08-06 14:53:42 -0300

    cambio config server

--- shared/src/shared_utils.c
+++ shared/src/shared_utils.c
@@ -680,7 +680,7 @@ int crear_conexion(char *ip, char* puerto)
 
 
 	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_UNSPEC;
+	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
 
